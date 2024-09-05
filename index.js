6b145d5eed5867a450c60ea9d5687825a4cbb343
Fauzan Azmi <fauzan.azmi01@gmail.com>
2024-09-05 11:07:11 +0700

    solve problems

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const collectionNumber = [1, 2, 3, 4, 5, 6]
  * Buat array baru berisi angka2 yg merupakan hasil perkalian 3 dari setiap element array collectionNumber
  */
 
+const newCollection = collectionNumber.map((val, idx, arr) => val * 3)
+console.log("New number collection:", newCollection)
 
 
 const colors = ["blue", "black", "purple", "white", "pink"]
 /**
  * buat array baru yang isinya element yang hurufnya lebih dari 4 berdasarkan array colors
  */
+
+const newColors = colors.filter((val, idx, arr) => val.length > 4)
+console.log("New colors:", newColors)
