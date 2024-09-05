cb02b4a5426adad5775d009d57caf34d8de18ec4
Fauzan Azmi <fauzan.azmi01@gmail.com>
2024-09-05 15:56:50 +0700

    initial commit

---
+++ homework.js
@@ -0,0 +1,55 @@
+const videoData = [
+  {
+    name: 'Miss Scarlet',
+    present: true,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      'dining room': false,
+      'billiard room': false,
+      library: false
+    }
+  },
+  {
+    name: 'Rusty',
+    present: false,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      'dining room': false,
+      'billiard room': false,
+      library: false
+    }
+  },
+  {
+    name: 'Colonel Mustard',
+    present: true,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      'dining room': false,
+      'billiard room': false,
+      library: false
+    }
+  },
+  {
+    name: 'Professor Plum',
+    present: true,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      'dining room': false,
+      'billiard room': false,
+      library: false
+    }
+  }
+]
+
+const presentVideoData = videoData.filter((item) => item.present)
+
+console.log("Present videos", presentVideoData)
+console.log("Count:", presentVideoData.length)
